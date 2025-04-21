0248c7d10102e8def4f920cc7e4f7ca0f13283b0
CosminBuzicUni <cosmin.buzic@student.nhlstenden.com>
2025-04-21 19:40:39 +0200

    made dummy login function

--- functions/index.js
+++ functions/index.js
@@ -9,42 +9,29 @@
 
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
-const functions = require("firebase-functions/v2");
-const express = require("express");
+// const functions = require("firebase-functions/v2");
+// const express = require("express");
 // const cors = require("cors");
 
 const admin = require("firebase-admin");
 
 admin.initializeApp();
 
-
-const app = express();
-
-app.get("/", async (req, res) => {
-  const snapshots = await admin.firestore().collection("users").get();
-  const users = [];
-  snapshots.forEach((doc) => {
-    const id = doc.id;
-    const data = doc.data();
-
-    users.push({id, ...data});
-  });
-
-  res.status(200).json(users);
-});
-
-app.post("/", async (req, res) => {
-  const user = req.body;
-  await admin.firestore().collection("users").add(user);
-  res.status(201).send();
-});
-
-exports.user = functions.https.onRequest(app);
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
 exports.helloWorld = onRequest((request, response) => {
   logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
+});
+
+exports.authenticateUser = onRequest((request, response) => {
+  const {userName, password} = request.body;
+  logger.info("Received authentication request", {userName, password});
+  // Perform authentication logic here
+  // For example, check against a database or an external service
+  const isAuthenticated = userName === "admin" && password === "password";
+  // Dummy check
+  if (isAuthenticated) {
+    response.status(200).send({message: "Authentication successful"});
+  } else {
+    response.status(401).send({message: "Authentication failed"});
+  }
 });
